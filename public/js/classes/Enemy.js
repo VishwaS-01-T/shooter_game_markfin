4c6827b4e69625f3524778cb4e9f8a833da8b158
Vishwas <vishwasdeepsingh1234@gmail.com>
2025-08-24 12:33:10 +0530

    Updated Enemy.js and added img folder

--- public/js/classes/Enemy.js
+++ public/js/classes/Enemy.js
@@ -5,12 +5,14 @@ class Enemy {
     this.radius = radius
     this.color = color
     this.velocity = velocity
+    // this.image = new Image()
   }
 
   draw() {
     c.beginPath()
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
     c.fillStyle = this.color
+    // c.drawImage(this.image, this.x - size/4 , this.y - size/4 , size*0.6, size*0.6)
     c.fill()
   }
 
@@ -20,3 +22,5 @@ class Enemy {
     this.y = this.y + this.velocity.y
   }
 }
+
+
